933282a668ae2cec9fed0ed5173720c4aace29ed
japipe05 <felipehuchija@gmail.com>
2022-11-06 15:34:30 -0500

    radicado

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ dotenv.config();
 const solicitudRouter = Router();
 let url = process.env.API_SEND_OTP;// 'http://localhost:8087/api/otp';
 let urllog = process.env.API_VAL_LOGIN; //'http://localhost:8087/api/login'
-let urlrad = process.env.API_VAL_RAD; //'http://localhost:8087/api/login'
+let urlrad = process.env.API_VAL_RAD; //'http://localhost:8087/api/solicitud'
 
+let gltocken = "";
 let transactionArr = []
 solicitudRouter.get('/home', (req, res) =>{
     res.render('home');
@@ -18,13 +19,13 @@ solicitudRouter.get('/solicitudotp', (req, res) =>{
 });
 
 solicitudRouter.post('/solicitudotp', (req, res) =>{
-    console.log(req.body.name);
+    //console.log(req.body.name);
     let data = {
         username : req.body.username,
         correo: req.body.correo
     }
     axios.post(url,data).then(response =>{
-        console.log(response);
+        //console.log(response);
     });
     
     res.render('login',{ success:'suc',correo:req.body.correo});
@@ -46,85 +47,71 @@ solicitudRouter.post('/login', async (req, res) =>{
         }
     )
     .catch((error) =>{
-        console.log("error");
         datos="err";
     });
     
-    console.log(req.body.username);
-    console.log(req.body.password);
+    //console.log(req.body.username);
+    //console.log(req.body.password);
     if (!datos){
-        console.log(response.data);
+        gltocken=response.data;
+        //console.log(gltocken);
         res.render('radicar');
     }else{
-        res.render('login',{ success:'err',correo:''});
+        res.render('login',{ success:'Err',correo:''});
     }
     
 });
 
 solicitudRouter.get('/radicar', (req, res) =>{
-    res.render('validaradicado');
+    res.render('validaradicado',{ correo:req.body.correo});
 });
 
 solicitudRouter.post('/radicar', (req, res) =>{
-   /* console.log(req.body.tipoDocumento);
-    console.log(req.body.numeroDocumento);
-    console.log(req.body.fechaExpedicion);
-    console.log(req.body.nombre1);
-    console.log(req.body.nombre2);
-    console.log(req.body.apellido1);
-    console.log(req.body.apellido2);
-    console.log(req.body.genero);
-    console.log(req.body.fechaNacimiento);
-    console.log(req.body.celular);
-    console.log(req.body.correoElectronico);
-    console.log(req.body.estadoCivil);
-    console.log(req.body.tipoInmueble);
-    console.log(req.body.tipoResidencia);
-    console.log(req.body.tipoContrato);
-    console.log(req.body.autorizaCentrales);
-    console.log(req.body.salarioMensual);
-    console.log(req.body.nivelEstudios);
-    console.log(req.body.actividadEconomica);
-    console.log(req.body.totalActivos);
-    console.log(req.body.totalPasivos);
-    console.log(req.body.valorSolicitado);
-    console.log(req.body.gastos);
-    console.log(req.body.plazo);*/
-
+    let dtfechaExpedicion = new Date(req.body.fechaExpedicion);
+    let dtfechaNacimiento = new Date(req.body.fechaNacimiento);
+    let accesotocken = gltocken;
+    //console.log(accesotocken.access_token);
+    let config ={
+        headers: { 
+            Authorization: "Bearer "+accesotocken.access_token,
+            'Content-Type': "application/json"        
+        }
+    }
 
     let data = {
-        tipoDocumento : req.body.tipoDocumento,
-        numeroDocumento : req.body.numeroDocumento,
-        fechaExpedicion : req.body.fechaExpedicion,
-        nombre1 : req.body.nombre1,
-        nombre2 : req.body.nombre2,
-        apellido1 : req.body.apellido1,
-        apellido2 : req.body.apellido2,
-        genero : req.body.genero,
-        fechaNacimiento : req.body.fechaNacimiento,
-        celular : req.body.celular,
-        correoElectronico : req.body.correoElectronico,
-        estadoCivil : req.body.estadoCivil,
-        tipoInmueble : req.body.tipoInmueble,
-        tipoResidencia : req.body.tipoResidencia,
-        tipoContrato : req.body.tipoContrato,
-        autorizaCentrales : req.body.autorizaCentrales,
-        salarioMensual : req.body.salarioMensual,
-        nivelEstudios : req.body.nivelEstudios,
-        actividadEconomica : req.body.actividadEconomica,
-        totalActivos : req.body.totalActivos,
-        totalPasivos : req.body.totalPasivos,
+        clienteDto: {
+            tipoDocumento : req.body.tipoDocumento,
+            numeroDocumento : req.body.numeroDocumento,
+            fechaExpedicion : dtfechaExpedicion,
+            nombre1 : req.body.nombre1,
+            nombre2 : req.body.nombre2,
+            apellido1 : req.body.apellido1,
+            apellido2 : req.body.apellido2,
+            genero : req.body.genero,
+            fechaNacimiento : dtfechaNacimiento,
+            celular : req.body.celular,
+            correoElectronico : req.body.correoElectronico,
+            estadoCivil : req.body.estadoCivil,
+            tipoInmueble : req.body.tipoInmueble,
+            tipoResidencia : req.body.tipoResidencia,
+            tipoContrato : req.body.tipoContrato,
+            autorizaCentrales : req.body.autorizaCentrales,
+            salarioMensual : req.body.salarioMensual,
+            nivelEstudios : req.body.nivelEstudios,
+            actividadEconomica : req.body.actividadEconomica,
+            totalActivos : req.body.totalActivos,
+            totalPasivos : req.body.totalPasivos,
+            gastos : req.body.gastos
+        },
         valorSolicitado : req.body.valorSolicitado,
-        gastos : req.body.gastos,
         plazo : req.body.plazo
     }
-    console.log(data); 
 
-    axios.post(urlrad,data).then(response =>{
-        console.log(response);
+    axios.post(urlrad,data,config).then(response =>{
+        console.log(config);
+        console.log(data);
     });
-    
-    res.render('validaradicado');
+    res.render('validaradicado',{ correo:req.body.correoElectronico});
 });
 
 export default solicitudRouter;
